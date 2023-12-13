628e9e47f6675f8eb9088fdb55f39efd3fb53711
Sina6464 <149200885+Sina6464@users.noreply.github.com>
2023-12-13 15:09:08 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Factorial
+{
+    static void Main()
+    {
+        //سینا شاهینی فاکتوریل
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        decimal factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine("Factorial of " + num + " is: " + factorial);
+        Console.ReadKey();
+    }
+}
